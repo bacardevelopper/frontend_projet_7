1d779287973ed70e4244f4da8194f506e328b192
bacardevelopper <bacar.darwin.github@hotmail.com>
2020-10-12 11:18:50 +0200

    message : en parellele version à 39%

--- src/app/ownpost/ownpost.component.ts
+++ src/app/ownpost/ownpost.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CookieService } from 'ngx-cookie-service';
+// importer http client
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-ownpost',
   templateUrl: './ownpost.component.html',
   styleUrls: ['./ownpost.component.scss']
 })
 export class OwnpostComponent implements OnInit {
-
-  constructor() { }
-
+  cookieValue: any = this.cookie.get('idusercookie');
+  constructor(private cookie : CookieService, private http : HttpClient) { }
+  ownPost(){
+    let formData = new FormData();
+    formData.append('cookie', JSON.stringify(this.cookieValue));
+    this.http.post('http://localhost:3000/home/own/post', formData).subscribe(
+      (reponse) =>{
+        console.log(reponse);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
   ngOnInit(): void {
+    this.ownPost();
   }
 
 }
